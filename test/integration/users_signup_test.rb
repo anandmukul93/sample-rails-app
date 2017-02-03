301c5778205de01480cba40501212e20721e63b1
Anand <anandmukul93@gmail.com>
2017-02-03 19:34:37 +0530

    sign-up form complete phase 1

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+    test "user should not signup with invalid form data" do
+        get signup_path
+        assert_select 'form[action="'+signup_path+'"]'
+        assert_no_difference 'User.count' do
+            post signup_path,
+                 params: { user: {
+                     name: "ana",
+                     email: "anandmukul93@gmail.com",
+                     password: "123",
+                     password_confirmation: "123"
+                     }
+                 }
+        end
+        assert_template 'users/new'
+        # assert_select "li.error-message", "Email has already been taken"
+        assert_select "li.error-message", "Password is too short (minimum is 7 characters)"
+        assert_select "li.error-message", "Password confirmation doesn't match Password"
+    end
+
+    test "valid user signup" do
+        get signup_path
+        assert_select "form[action=\"#{signup_path}\"]"
+        assert_difference 'Users.count', 1 do
+            post signup_path, params: { user: { name:  "Example User",
+                                               email: "user@example.com",
+                                               password:              "password",
+                                               password_confirmation: "password" } }
+        end
+        follow_redirect!
+        assert_not flash.empty?
+        assert_template 'users/show'
+    end
+end
