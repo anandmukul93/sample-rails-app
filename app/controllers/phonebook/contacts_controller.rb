111b217095dcb4930814e4462ddfe81516784957
Anand <anandmukul93@gmail.com>
2017-02-10 22:05:06 +0530

    search page made

--- app/controllers/phonebook/contacts_controller.rb
+++ app/controllers/phonebook/contacts_controller.rb
@@ -1,11 +1,16 @@
 class Phonebook::ContactsController < ApplicationController
+	layout "phonebook"
+
     def search
 
     end
 
     def add
-
+		#find the books and response
     end
 
+    def find
+		#find the books and return as json the response
+	end
 
 end
